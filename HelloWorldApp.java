33f9acb6c4a510186c2bb523f395140da3f5599b
Arpan-dev-coder <tantubaiarpan@gmail.com>
2026-03-11 15:50:04 +0530

    UC2 implemented: Greeting using command-line argument

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -1,9 +1,19 @@
+
 public class HelloWorldApp {
 
     public static void main(String[] args) {
 
-        // Print message to console
-        System.out.println("Hello World");
+        // Check if name provided
+        if (args.length > 0) {
 
+            String name = args[0];
+
+            System.out.println("Hello " + name);
+
+        } else {
+
+            System.out.println("Hello User");
+
+        }
     }
 }
